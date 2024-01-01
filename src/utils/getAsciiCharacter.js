63caa50337927bc9eb5519aab97d3703b6963dfa
sim <s_bullado51@hotmail.com>
2024-01-01 02:33:09 +0100

    Extract pixel to ASCII conversion logic into getAsciiCharacter

--- src/utils/getAsciiCharacter.js
+++ src/utils/getAsciiCharacter.js
@@ -1,3 +1,18 @@
+/**
+ * Calculates the corresponding ASCII character from the RGB color components of a pixel.
+ *
+ * This function first calculates the average brightness of the pixel from its components
+ * red (r), green (g) and blue (b). Then, it uses this luminosity to find a
+ * matching character in ASCII density string. Brighter pixels
+ * correspond to lighter characters in the density string, and higher pixels
+ * dark to darker characters.
+ *
+ * @param {number} r - The red component of the pixel, between 0 and 255.
+ * @param {number} g - The green component of the pixel, between 0 and 255.
+ * @param {number} b - The blue component of the pixel, between 0 and 255.
+ * @returns {string} The ASCII character corresponding to the brightness of the pixel.
+ */
+
 import mapValue from "./mapValue";
 import { density } from "../config";
 
