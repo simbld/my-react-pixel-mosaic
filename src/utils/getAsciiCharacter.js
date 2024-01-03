99660274e79fc9094cdb7aa841e60847dfdf73de
sim <s_bullado51@hotmail.com>
2024-01-03 06:50:07 +0100

    Fix ESLint issues for code quality improvement

--- src/utils/getAsciiCharacter.js
+++ src/utils/getAsciiCharacter.js
@@ -16,8 +16,10 @@
 import mapValue from "./mapValue";
 import { density } from "../config";
 
-export function getAsciiCharacter(r, g, b) {
+function getAsciiCharacter(r, g, b) {
   const avg = (r + g + b) / 3;
   const charIndex = Math.floor(mapValue(avg, 0, 255, density.length, 0));
   return density.charAt(charIndex);
 }
+
+export default getAsciiCharacter;
