38baa51b809235bd8529aea8b159a4f6387d98aa
simbld <simon-bullado_student2022@wilder.school>
2024-01-28 23:59:31 +0100

    Add other density string

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const TARGET_WIDTH = 80;
 export const TARGET_HEIGHT = 80;
-export const density = `${"\u2591"}${"\u2592"}${"\u2593"}${"\u2588"}`;
+export const density = `${'\u2591'}${'\u2592'}${'\u2593'}${'\u2588'}`;
+// (n@#w¤9876543210?!abc;:+=-,.| ')
