d7d7a3c468c182db46c2c6504184d9bcc28b6a93
simbld <simon-bullado_student2022@wilder.school>
2024-03-03 17:02:52 +0100

    merge main

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,23 +1,41 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import romeo from "../assets/romeo.jpg";
 import { TARGET_WIDTH, TARGET_HEIGHT } from "../config";
-import { createCanvas } from "../utils/createCanvas";
-import { generateAsciiArt } from "../utils/generateAsciiArt";
+import createCanvas from "../utils/createCanvas";
+import generateAsciiArt from "../utils/generateAsciiArt";
+import generateStipplingEffect from "../utils/generateStipplingEffect";
 
 function Picture() {
-  const [asciiArt, setAsciiArt] = useState("");
+  const [filterOutput, setFilterOutput] = useState("");
+  const [filterType, setFilterType] = useState("ascii");
 
   useEffect(() => {
     const image = new Image();
     image.src = romeo;
     image.onload = () => {
       const { imageData } = createCanvas(image, TARGET_HEIGHT, TARGET_WIDTH);
-      const ascii = generateAsciiArt(imageData);
-      setAsciiArt(ascii);
+      if (filterType === "ascii") {
+        const ascii = generateAsciiArt(imageData);
+        setFilterOutput(ascii);
+      } else if (filterType === "stippling") {
+        const stippling = generateStipplingEffect(imageData);
+        setFilterOutput(stippling);
+      }
     };
-  }, []);
+  }, [filterType]);
 
-  return <div className="asciiArt">{asciiArt}</div>;
+  return (
+    <div>
+      <select
+        onChange={(e) => setFilterType(e.target.value)}
+        value={filterType}
+      >
+        <option value="ascii">Art ASCII</option>
+        <option value="stippling">Effet de Stippling</option>
+      </select>
+      <div className="filterOutput">{filterOutput}</div>
+    </div>
+  );
 }
 
 export default Picture;
