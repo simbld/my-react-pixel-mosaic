7369ea8dfd8ac4c943c042e618b230abaeec7c64
simbld <simon-bullado_student2022@wilder.school>
2024-03-03 17:02:52 +0100

    merge main

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,45 +1,41 @@
-import { useState, useEffect } from 'react';
-import romeo from '../assets/romeo.jpg';
-import { TARGET_WIDTH, TARGET_HEIGHT } from '../config';
-import createCanvas from '../utils/createCanvas';
-import generateAsciiArt from '../utils/generateAsciiArt';
-import generateStipplingEffect from '../utils/generateStipplingEffect';
+import { useState, useEffect } from "react";
+import romeo from "../assets/romeo.jpg";
+import { TARGET_WIDTH, TARGET_HEIGHT } from "../config";
+import createCanvas from "../utils/createCanvas";
+import generateAsciiArt from "../utils/generateAsciiArt";
+import generateStipplingEffect from "../utils/generateStipplingEffect";
 
 function Picture() {
-	const [filterOutput, setFilterOutput] = useState('');
-	const [filterType, setFilterType] = useState('ascii');
+  const [filterOutput, setFilterOutput] = useState("");
+  const [filterType, setFilterType] = useState("ascii");
 
-	useEffect(() => {
-		const image = new Image();
-		image.src = romeo;
-		image.onload = () => {
-			const { imageData } = createCanvas(
-				image,
-				TARGET_HEIGHT,
-				TARGET_WIDTH,
-			);
-			if (filterType === 'ascii') {
-				const ascii = generateAsciiArt(imageData);
-				setFilterOutput(ascii);
-			} else if (filterType === 'stippling') {
-				const stippling = generateStipplingEffect(imageData);
-				setFilterOutput(stippling);
-			}
-		};
-	}, [filterType]);
+  useEffect(() => {
+    const image = new Image();
+    image.src = romeo;
+    image.onload = () => {
+      const { imageData } = createCanvas(image, TARGET_HEIGHT, TARGET_WIDTH);
+      if (filterType === "ascii") {
+        const ascii = generateAsciiArt(imageData);
+        setFilterOutput(ascii);
+      } else if (filterType === "stippling") {
+        const stippling = generateStipplingEffect(imageData);
+        setFilterOutput(stippling);
+      }
+    };
+  }, [filterType]);
 
-	return (
-		<div>
-			<select
-				onChange={(e) => setFilterType(e.target.value)}
-				value={filterType}
-			>
-				<option value="ascii">Art ASCII</option>
-				<option value="stippling">Effet de Stippling</option>
-			</select>
-			<div className="filterOutput">{filterOutput}</div>
-		</div>
-	);
+  return (
+    <div>
+      <select
+        onChange={(e) => setFilterType(e.target.value)}
+        value={filterType}
+      >
+        <option value="ascii">Art ASCII</option>
+        <option value="stippling">Effet de Stippling</option>
+      </select>
+      <div className="filterOutput">{filterOutput}</div>
+    </div>
+  );
 }
 
 export default Picture;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const TARGET_WIDTH = 80;
 export const TARGET_HEIGHT = 80;
-export const density = `${'\u2591'}${'\u2592'}${'\u2593'}${'\u2588'}`;
+export const density = `${"\u2591"}${"\u2592"}${"\u2593"}${"\u2588"}`;
 
 // "Ñ@#W$9876543210?!abc;:+=-,._          "
 // '       .:-i|=+╦%O#@'
@@ -8,4 +8,3 @@ export const density = `${'\u2591'}${'\u2592'}${'\u2593'}${'\u2588'}`;
 // loadJSON("https://api.weather.gov/gridpoints/LWX/108,90/forecast/hourly");
 
 // (n@#w¤9876543210?!abc;:+=-,.| ')
-
