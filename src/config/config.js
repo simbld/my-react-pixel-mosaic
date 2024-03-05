bb9adf2bd3675f7e9ae63cf0a159f2b87ab81a3e
simbld <simon-bullado_student2022@wilder.school>
2024-03-05 07:08:44 +0100

    update: reorganization of the config file

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ export const TARGET_HEIGHT = 80;
 export const density = `${"\u2591"}${"\u2592"}${"\u2593"}${"\u2588"}`;
 
 // "Ñ@#W$9876543210?!abc;:+=-,._          "
-// '       .:-i|=+╦%O#@'
+// ' .:-i|=+╦%O#@'
 // '        .:░▒▓█'
 // loadJSON("https://api.weather.gov/gridpoints/LWX/108,90/forecast/hourly");
 
