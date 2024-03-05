a089246c57ed9e41b806b7f09227ae2ea1de04b5
simbld <simon-bullado_student2022@wilder.school>
2024-03-05 06:59:39 +0100

    config: updated Vite, Babel, Webpack configuration and typescript configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
-const path = require("path");
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./src/main.jsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"]
+  },
   module: {
     rules: [
       {
-        test: /\.less$/,
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-typescript"]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "less-loader"
+            loader: "file-loader"
           }
         ]
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
       }
     ]
   },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ],
+  devServer: {
+    hot: true,
+    open: true
   }
 };
