5aaacca5543e21509811d1266ba5f9835aa2d3da
simbld <simon-bullado_student2022@wilder.school>
2024-03-11 23:38:08 +0100

    update: config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base: ".",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
   build: {
     outDir: "build",
     assetsInlineLimit: 0,
