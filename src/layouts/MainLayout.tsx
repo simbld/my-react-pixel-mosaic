c316145e4c42f1fe7c1a64c9e3943d4a60efe9dd
simbld <simon-bullado_student2022@wilder.school>
2024-03-15 12:40:53 +0100

    feat: Render image only filtered

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,51 +1,34 @@
 import { useEffect, useRef } from "react";
 import {
-  TARGET_HEIGHT,
-  TARGET_WIDTH,
   defaultImage,
-  densityDot
+  densityDot,
+  TARGET_WIDTH,
+  TARGET_HEIGHT
 } from "../config/config";
 
 const MainLayout: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const imageRef = useRef<HTMLImageElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const image = imageRef.current;
-    if (!canvas || !image) return;
-
-    const context = canvas.getContext("2d");
-    if (!context) return;
+    const canvas = canvasRef.current!;
+    const context = canvas.getContext("2d")!;
+    const image = imageRef.current!;
 
     image.onload = () => {
-      const scaleFactor = 0.1;
-      const scaledWidth = image.width * scaleFactor;
-      const scaledHeight = image.height * scaleFactor;
-
+      // Définir la taille du canevas
       canvas.width = TARGET_WIDTH;
       canvas.height = TARGET_HEIGHT;
 
-      context.clearRect(0, 0, canvas.width, canvas.height);
+      // Dessiner l'image sur le canevas à la taille du canevas
       context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      // Récupérer les données de l'image
       const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
       const data = imageData.data;
 
-      for (let i = 0; i < canvas.width; i++) {
-        for (let j = 0; j < canvas.height; j++) {
-          const pixelIndex = (i + j * canvas.width) * 4;
-          const r = data[pixelIndex + 0];
-          const g = data[pixelIndex + 1];
-          const b = data[pixelIndex + 2];
-          const avg = (r + g + b) / 3;
-
-          data[pixelIndex + 0] = avg;
-          data[pixelIndex + 1] = avg;
-          data[pixelIndex + 2] = avg;
-        }
-      }
-
-      context.putImageData(imageData, 0, 0);
+      // Effacer le canevas avant de dessiner l'image ASCII
+      context.clearRect(0, 0, canvas.width, canvas.height);
 
       for (let i = 0; i < canvas.width; i += 10) {
         for (let j = 0; j < canvas.height; j += 10) {
