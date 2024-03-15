d59554489965f8138746c199a50ac9b0ef830dd4
simbld <simon-bullado_student2022@wilder.school>
2024-03-15 09:01:07 +0100

    feat: Add the useMapValue function to get the ASCII character corresponding to the given brightness

--- src/helpers/getAsciiCharacter.ts
+++ src/helpers/getAsciiCharacter.ts
@@ -1,10 +1,19 @@
+/**
+ *
+ * @param brightness
+ *
+ * @returns The ASCII character corresponding to the given brightness.
+ *
+ */
+
 import { densityDot } from "../config/config";
+import useMapValue from "../hooks/useMapValue";
 
 function getAsciiCharacter(brightness: number): string {
   const chars = densityDot.split("");
-  const index = Math.floor(((brightness - 0) * (chars.length - 1)) / (255 - 0));
+  const index = useMapValue(brightness, 0, 255, 0, chars.length - 1);
 
-  return chars[index];
+  return chars[Math.floor(index)];
 }
 
 export default getAsciiCharacter;
