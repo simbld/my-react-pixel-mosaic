0c5425736a32a4da1ab90dc9e86d0cc197c165ac
simbld <simon-bullado_student2022@wilder.school>
2024-03-17 05:38:55 +0100

    feat: Updating rootReducer with localforage and redux-persist

--- src/features/reducers/rootReducer.ts
+++ src/features/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
+import { Reducer, combineReducers } from "@reduxjs/toolkit";
 import gameboyReducer from "./gameboy/gameboySlice";
-import { combineReducers, Reducer } from "redux";
 import { imageProcessingReducer } from "./imageprocessing/imageProcessingSlice";
 
 const rootReducer: Reducer = combineReducers({
   imageProcessing: imageProcessingReducer,
   gameboy: gameboyReducer
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
