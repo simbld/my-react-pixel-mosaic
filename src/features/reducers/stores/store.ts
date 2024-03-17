b9c01d335bc90d4e7ced2689e06fcfae76b81348
simbld <simon-bullado_student2022@wilder.school>
2024-03-17 05:39:46 +0100

    feat: Updating Redux store configuration with redux-persist and localforage

--- src/features/reducers/stores/store.ts
+++ src/features/reducers/stores/store.ts
@@ -1,20 +1,49 @@
-import { combineReducers, configureStore, Store } from "@reduxjs/toolkit";
+import { configureStore, Store } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
-import { imageProcessingReducer } from "../imageprocessing/imageProcessingSlice";
-import gameboyReducer from "../gameboy/gameboySlice";
+import rootReducer from "../../reducers/rootReducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from "redux-persist/lib/constants";
 
-const rootReducer = combineReducers({
-  imageProcessing: imageProcessingReducer,
-  gameboy: gameboyReducer
+// Configuration de redux-persist
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["imageProcessing", "gameboy"]
+};
+
+// Création du store redux avec redux-persist
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store: Store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat(thunk) // thunk est un middleware supplémentaire
 });
 
-const store: Store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
-});
+// Initialisation de redux-persist avec le store configuré
+export const persistor = persistStore(store);
 
+// Définition du type pour le store Redux utilisé dans l'application
 export type AppStore = typeof store;
+
+// Type pour l'état global avec useSelector
 export type RootState = ReturnType<typeof store.getState>;
+
+// Type avec useDispatch
 export type AppDispatch = typeof store.dispatch;
 
+// Export par défaut du store configuré
 export default store;
