92e7ab1f67e7754c82c5fad3f431b40da44c97d5
simbld <simon-bullado_student2022@wilder.school>
2024-03-23 12:17:49 +0100

    feat: Replacement of Tablet component with Gameboy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import ErrorMessage from "./common/ErrorMessage";
-import Tablet from "./components/Tablet";
+import Gameboy from "./components/Gameboy";
 import { defaultImage } from "./config/config";
 import { ErrorProvider } from "./contexts/ErrorContext";
+import "./styles/less/index.less";
 
 const App: React.FC = () => {
+  const [showGameboy, setShowGameboy] = useState(false);
+
   return (
     <>
       <ErrorProvider>
-        <Tablet imageSrc={defaultImage} />
+        {!showGameboy ? (
+          <button
+            type="button"
+            className="title-btn"
+            onClick={() => setShowGameboy(true)}
+          >
+            <span className="title-text">RePixelAct</span>
+          </button>
+        ) : (
+          <Gameboy imageSrc={defaultImage} />
+        )}
         <ErrorMessage />
       </ErrorProvider>
     </>
