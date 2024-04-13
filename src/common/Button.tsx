b1882678d8a47333e7e869260585e9c39e01f2ba
simbld <simon-bullado_student2022@wilder.school>
2024-04-13 23:51:00 +0200

    feat: Button component update and end

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -1,13 +1,29 @@
 import type { ButtonProps } from "../interfaces/types";
+import { useState } from "react";
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, loading = false }) => {
+  const [textOpacity, setTextOpacity] = useState(1);
+  const [textHoverOpacity, setTextHoverOpacity] = useState(0);
+
   return (
-    <button className="title-btn" onClick={onClick} disabled={loading}>
+    <button
+      className="title-btn"
+      onClick={onClick}
+      disabled={loading}
+      onMouseEnter={() => {
+        setTextHoverOpacity(1);
+        setTextOpacity(0);
+      }}
+      onMouseLeave={() => {
+        setTextHoverOpacity(0);
+        setTextOpacity(1);
+      }}
+    >
       <svg className="text-svg" viewBox="0 0 1200 300" overflow="visible">
         <defs>
           <linearGradient id="text-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" stopColor="lightgrey" />
-            <stop offset="100%" stopColor="dark" />
+            <stop offset="0%" stopColor="darkblue" />
+            <stop offset="100%" stopColor="lightblue" />
           </linearGradient>
           <linearGradient
             id="text-gradient-hover"
@@ -16,7 +32,7 @@ const Button: React.FC<ButtonProps> = ({ text, onClick, loading = false }) => {
             x2="0%"
             y2="100%"
           >
-            <stop offset="0%" stopColor="black" />
+            <stop offset="0%" stopColor="lightblue" />
             <stop offset="100%" stopColor="darkblue" />
           </linearGradient>
           <linearGradient id="arrow-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
@@ -31,91 +47,97 @@ const Button: React.FC<ButtonProps> = ({ text, onClick, loading = false }) => {
             y2="100%"
           >
             <stop offset="0%" stopColor="lightgrey" />
-            <stop offset="50%" stopColor="white" />
-            <stop offset="100%" stopColor="lightgrey" />
+            <stop offset="100%" stopColor="darkgrey" />
           </linearGradient>
         </defs>
         <g transform="skewX(35)">
           <rect
+            className="arrow-group-1"
             x="150"
             y="25"
             width="650"
             height="250"
             fill="url(#arrow-gradient)"
             stroke="url(#border-gradient)"
-            strokeWidth="5"
+            strokeWidth="10"
           />
           <rect
+            className="arrow-group-2"
             x="816"
             y="25"
             width="80"
             height="250"
             fill="url(#arrow-gradient)"
             stroke="url(#border-gradient)"
-            strokeWidth="5"
+            strokeWidth="10"
           />
           <rect
+            className="arrow-group-3"
             x="912"
             y="25"
             width="40"
             height="250"
             fill="url(#arrow-gradient)"
             stroke="url(#border-gradient)"
-            strokeWidth="5"
+            strokeWidth="10"
           />
           <rect
+            className="arrow-group-4"
             x="968"
             y="25"
             width="25"
             height="250"
             fill="url(#arrow-gradient)"
             stroke="url(#border-gradient)"
-            strokeWidth="5"
+            strokeWidth="10"
           />
           <rect
+            className="arrow-group-5"
             x="1009"
             y="25"
             width="15"
             height="250"
             fill="url(#arrow-gradient)"
             stroke="url(#border-gradient)"
-            strokeWidth="5"
+            strokeWidth="10"
           />
         </g>
         <polygon
+          className="arrow-group-0"
           points="-10,90 40,-40 70,25 150,25 327,275 205,275 230,340"
           fill="url(#arrow-gradient)"
           stroke="url(#border-gradient)"
-          strokeWidth="5"
+          strokeWidth="10"
         />
         <text
           className="button-text"
-          x="570"
-          y="150"
-          fontSize="120"
+          x="585"
+          y="160"
+          fontSize="110"
           fontFamily="'Back', sans-serif"
           fill="url(#text-gradient)"
           stroke="url(#border-gradient)"
           strokeWidth="5"
           textAnchor="middle"
           dominantBaseline="central"
+          style={{ opacity: textOpacity }}
         >
           {text}
         </text>
         <text
           className="button-text-hover"
-          x="570"
+          x="530"
           y="150"
-          fontSize="110"
+          fontSize="120"
           fontFamily="'Back', sans-serif"
           fill="url(#text-gradient-hover)"
           stroke="url(#border-gradient)"
           strokeWidth="5"
           textAnchor="middle"
           dominantBaseline="central"
-          style={{ opacity: 0 }}
+          style={{ opacity: textHoverOpacity }}
         >
-          {text}
+          ENTER
         </text>
       </svg>
     </button>
