0f947f40e926609e2f60f696700b99a2c8fa4ce5
simbld <simon-bullado_student2022@wilder.school>
2024-06-03 09:41:16 +0200

    feat: Add getDefaultDensity function to retrieve default density based on filter type

---
+++ src/features/utils/density/GetDefaultDensity.ts
@@ -0,0 +1,12 @@
+import {
+  asciiDensitySimple,
+  stipplingDensitySimple
+} from "../../../config/config";
+
+export const getDefaultDensity = (filterType: string) => {
+  if (filterType === "ascii") return asciiDensitySimple;
+  if (filterType === "stippling") return stipplingDensitySimple;
+  return asciiDensitySimple;
+};
+
+// Path: src/features/utils/density/GetDefaultDensity.ts
