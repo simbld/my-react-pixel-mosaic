c7e1eca1e80c5989bccf516a30edd9ed59ad850e
simbld <simon-bullado_student2022@wilder.school>
2024-08-23 21:33:02 +0200

    chore: Add rangeSliderReducer and filtersReducer to rootReducer

--- src/features/reducers/rootReducer.ts
+++ src/features/reducers/rootReducer.ts
@@ -2,13 +2,17 @@ import { Reducer, combineReducers } from "@reduxjs/toolkit";
 import gameboyReducer from "./gameboy/gameboySlice";
 import { imageProcessingReducer } from "./imageprocessing/imageProcessingSlice";
 import menuGameboyReducer from "./menugameboy/menuGameboySlice";
+import rangeSliderReducer from "./rangeSliders/rangeSliderSlice";
+import { filtersReducer } from "./filters/filtersSlice";
 
 const rootReducer: Reducer = combineReducers({
   imageProcessing: imageProcessingReducer,
   gameboy: gameboyReducer,
-  menuGameboy: menuGameboyReducer
+  menuGameboy: menuGameboyReducer,
+  rangeSliders: rangeSliderReducer,
+  filters: filtersReducer
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootStateProps = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
