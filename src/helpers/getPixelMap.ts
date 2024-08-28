c2fc5f970f06f3af4e1bddc904103a92846f4ea0
simbld <simon-bullado_student2022@wilder.school>
2024-08-28 23:46:30 +0200

    chore: Update getPixelMap function to use start and stop instead of x1, y1, x2, y2

--- src/helpers/getPixelMap.ts
+++ src/helpers/getPixelMap.ts
@@ -1,11 +1,17 @@
-function getPixelMap(
-  value: number,
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number
-): number {
-  return ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
-}
+import type { GetPixelMapProps } from "@interfaces/types";
 
+/**
+ * Mappe une valeur d'un intervalle à un autre.
+ * @param {GetPixelMapProps} props - Les propriétés pour mapper la valeur.
+ * @returns {number} - La valeur mappée.
+ */
+const getPixelMap = ({
+  value,
+  start1,
+  stop1,
+  start2,
+  stop2
+}: GetPixelMapProps): number => {
+  return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+};
 export default getPixelMap;
